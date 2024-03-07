3d0cfb08c2eea3a97d8d7951d2544b350a30bca4
Andrey <andydeshko@gmail.com>
2024-03-07 23:10:25 +0200

    mod the main page responsivness

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,14 +1,16 @@
 import NavBar from "./NavBar";
 
 const Home: React.FC = () => {
-  return (
-    <div>
-      <NavBar />
-      
-      <h1>Home</h1>
-      
-    </div>
-  )
-}
+    return (
+        <div>
+            <NavBar />
+            <div className="home-container">
+                <h1>Welcome to JourneyLingua</h1>
+                <p>Start your language learning journey today!</p>
+                <button className="start-learning-button">Start Learning</button>
+            </div>
+        </div>
+    );
+};
 
 export default Home;
