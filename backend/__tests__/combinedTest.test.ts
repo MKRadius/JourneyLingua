2ff298d793c30e355acd8e241cdafccd2c8fa8dd
Andrey <andydeshko@gmail.com>
2024-03-13 22:07:30 +0200

    test report for each test in combinedTest generates

--- backend/__tests__/combinedTest.test.ts
+++ backend/__tests__/combinedTest.test.ts
@@ -1,163 +1,82 @@
 import request from 'supertest';
 import app from '../server';
 
-
 describe('Combined Test Including Language Creation', () => {
-  it('should create all entities sequentially: 1. New language 2. New Exercise 3. New MakeASentenceEx 4. New ImageToTextEx', async () => {
-    // Step 1: Create a new language (Spanish)
-    let newLanguage = {
-      languageName: 'Spanish',
-    };
+    let languageId: number = 1;
+    const exerciseId: number = 1000;
 
-    let response = await request(app)
-      .post('/language')
-      .send(newLanguage);
+    describe('Adding new language to the Language table', () => {
+        beforeAll(async () => {
+            // Step 1: Create a new language (Spanish)
+            const newLanguage = {
+                languageName: 'Spanish',
+            };
 
-    // Verify that the language creation was successful
-    expect(response.statusCode).toBe(201);
-    expect(response.body).toHaveProperty('languageName', newLanguage.languageName);
+            const response = await request(app)
+                .post('/language')
+                .send(newLanguage);
 
-    // Predefined languageId for Finnish for the next steps
-    const languageId = 1;
+            expect(response.statusCode).toBe(201);
+            expect(response.body).toHaveProperty('languageName', newLanguage.languageName);
+        });
 
-    // Step 2: Create a new exercise using Finnish
-    response = await request(app)
-      .post('/exercise')
-      .send({
-        exerciseId: 100,
-        languageId,
-      });
+        it('should create a new language in the Language table', () => {
 
-    // Verify that the exercise creation was successful
-    expect(response.statusCode).toBe(201);
-    expect(response.body).toHaveProperty('message', 'Exercise created successfully');
+            expect(languageId).toBeDefined();
+            expect(languageId).toBeGreaterThan(0);
+        });
+    });
 
-    // Step 3: Create a new MakeASentenceEx using Finnish
-    const exerciseId = 100;
-    response = await request(app)
-      .post('/lesson/makeASentenceEx')
-      .send({
-        exerciseId,
-        languageId,
-        sentenceEng: 'I love to play football',
-        sentenceFin: 'Minä rakastan jalkapallon pelaamista',
-      });
+    describe('Create Exercise', () => {
+        it('should create a new exercise', async () => {
+            // Step 2: Create a new exercise using the created language
+            const response = await request(app)
+                .post('/exercise')
+                .send({
+                    exerciseId,
+                    languageId,
+                });
 
-    // Verify that the MakeASentenceEx creation was successful
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toHaveProperty('message', 'MakeASentenceEx created successfully');
+            expect(response.statusCode).toBe(201);
+            expect(response.body).toHaveProperty('message', 'Exercise created successfully');
+        });
+    });
 
-    // Step 4: Create a new ImageToTextEx using Finnish
-    response = await request(app)
-      .post('/lesson/imageToTextEx')
-      .send({
-        wordEng: 'apple',
-        wordFin: 'omena',
-        category: 'fruit',
-        imageLink: 'http://example.com/apple.jpg',
-        exerciseId,
-        languageId,
-      });
 
-    // Verify that the ImageToTextEx creation was successful
-    expect(response.statusCode).toBe(201);
-    expect(response.body).toHaveProperty('wordEng', 'apple');
-  });
+    describe('MakeASentenceEx Creation', () => {
+        it('should create a new MakeASentenceEx', async () => {
+            // Step 3: Create a new MakeASentenceEx using the created language
+            const response = await request(app)
+                .post('/lesson/makeASentenceEx')
+                .send({
+                    exerciseId,
+                    languageId,
+                    sentenceEng: 'I love Typescript',
+                    sentenceFin: 'Minä rakastan Typescriptista',
+                });
+
+            expect(response.statusCode).toBe(200);
+            expect(response.body).toHaveProperty('message', 'MakeASentenceEx created successfully');
+        });
+    });
+
+    describe('ImageToTextEx Creation', () => {
+        it('should create a new ImageToTextEx', async () => {
+            // Step 4: Create a new ImageToTextEx using the created language
+            const response = await request(app)
+                .post('/lesson/imageToTextEx')
+                .send({
+                    wordEng: 'cucumber',
+                    wordFin: 'kurkku',
+                    category: 'food',
+                    imageLink: 'http://example.com/apple.jpg',
+                    exerciseId,
+                    languageId,
+                });
+
+            expect(response.statusCode).toBe(201);
+            expect(response.body).toHaveProperty('wordEng', 'cucumber');
+        });
+    });
+
 });
-
-
-
-
-// ----------
-
-// import request from 'supertest';
-// import app from '../server';
-//
-// describe('Combined Test Including Language Creation', () => {
-//     let createdEntities: any = []; // Array to store IDs of created entities
-//
-//
-//
-//     it('should create all entities sequentially: 1. New language 2. New Exercise 3. New MakeASentenceEx 4. New ImageToTextEx', async () => {
-//         // Step 1: Create a new language (Spanish)
-//         let newLanguage = {
-//             languageName: 'Spanish',
-//         };
-//
-//         let response = await request(app)
-//             .post('/language')
-//             .send(newLanguage);
-//
-//         // Verify that the language creation was successful
-//         expect(response.statusCode).toBe(201);
-//         expect(response.body).toHaveProperty('languageName', newLanguage.languageName);
-//
-//         // Store the ID of the created language for cleanup
-//         createdEntities.push({ type: 'language', id: response.body.languageId });
-//
-//         // Predefined languageId for Finnish for the next steps
-//         const languageId = 1;
-//
-//         // Step 2: Create a new exercise using Finnish
-//         response = await request(app)
-//             .post('/exercise')
-//             .send({
-//                 exerciseId: 100,
-//                 languageId,
-//             });
-//
-//         // Verify that the exercise creation was successful
-//         expect(response.statusCode).toBe(201);
-//         expect(response.body).toHaveProperty('message', 'Exercise created successfully');
-//
-//         // Store the ID of the created exercise for cleanup
-//         createdEntities.push({ type: 'exercise', id: response.body.exerciseId });
-//
-//         // Step 3: Create a new MakeASentenceEx using Finnish
-//         const exerciseId = 100;
-//         response = await request(app)
-//             .post('/lesson/makeASentenceEx')
-//             .send({
-//                 exerciseId,
-//                 languageId,
-//                 sentenceEng: 'I love to play football',
-//                 sentenceFin: 'Minä rakastan jalkapallon pelaamista',
-//             });
-//
-//         // Verify that the MakeASentenceEx creation was successful
-//         expect(response.statusCode).toBe(200);
-//         expect(response.body).toHaveProperty('message', 'MakeASentenceEx created successfully');
-//
-//         // Store the ID of the created MakeASentenceEx for cleanup
-//         createdEntities.push({ type: 'MakeASentenceEx', id: response.body.id });
-//
-//         // Step 4: Create a new ImageToTextEx using Finnish
-//         response = await request(app)
-//             .post('/lesson/imageToTextEx')
-//             .send({
-//                 wordEng: 'apple',
-//                 wordFin: 'omena',
-//                 category: 'fruit',
-//                 imageLink: 'http://example.com/apple.jpg',
-//                 exerciseId,
-//                 languageId,
-//             });
-//
-//         // Verify that the ImageToTextEx creation was successful
-//         expect(response.statusCode).toBe(201);
-//         expect(response.body).toHaveProperty('wordEng', 'apple');
-//
-//         // Store the ID of the created ImageToTextEx for cleanup
-//         createdEntities.push({ type: 'ImageToTextEx', id: response.body.id });
-//
-//
-//         afterAll(async () => {
-//             // Loop through the created entities and send requests to delete them
-//             for (let entity of createdEntities) {
-//                 await request(app)
-//                     .delete(`/delete/${entity.type}/${entity.id}`) // Adjust the endpoint as per your server implementation
-//                     .expect(200); // Assuming a successful deletion returns status code 200
-//             }
-//         });
-//     });
-// });
